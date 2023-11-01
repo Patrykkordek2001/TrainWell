7e47100fce1c91db1e065ceac40c518c1d7800a0
patryk.kordek <patryk.kordek@idealogic.pl>
2023-11-01 20:59:02 +0100

    essa

--- TrainWell - FRONTEND/TrainWellApp/src/app/components/menu/menu.component.ts
+++ TrainWell - FRONTEND/TrainWellApp/src/app/components/menu/menu.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, forwardRef } from '@angular/core';
 import { Router } from '@angular/router';
-import { CalendarOptions } from '@fullcalendar/core';
+import { FullCalendarComponent } from '@fullcalendar/angular';
+import { Calendar, CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction'
+
 
 
 @Component({
@@ -9,17 +12,41 @@ import dayGridPlugin from '@fullcalendar/daygrid';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
 
 
-  calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin]
-  };
-    constructor(private router: Router) {
-      
-    }
-  
-    
+  calendarOptions?: CalendarOptions;
+  eventsModel: any;
+  @ViewChild('fullcalendar') fullcalendar?: FullCalendarComponent;
+
+  ngOnInit() {
+    // need for load calendar bundle first
+    forwardRef(() => Calendar);
+
+    this.calendarOptions = {
+      plugins: [dayGridPlugin, interactionPlugin],
+      editable: true,
+      customButtons: {
+        myCustomButton: {
+          text: 'custom!',
+          click: function () {
+            alert('clicked the custom button!');
+          }
+        }
+      },
+      headerToolbar: {
+        left: 'prev,next today myCustomButton',
+        center: 'title',
+        right: 'dayGridMonth'
+      },
+      dateClick: this.handleDateClick.bind(this),
+
+    };
+  }
+
+  handleDateClick(arg: DateClickArg) {
+    console.log(arg);
+    console.log(" ");
+  }
 
 }
